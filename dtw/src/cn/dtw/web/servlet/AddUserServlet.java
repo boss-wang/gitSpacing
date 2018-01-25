94fb64cac630e27c6a17db70603bb694bc413bb3
wangchao <261668497@qq.com>
2018-01-25 15:12:23 +0800

    添加用户的servlet

---
+++ dtw/src/cn/dtw/web/servlet/AddUserServlet.java
@@ -0,0 +1,26 @@
+package cn.dtw.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addUser")
+public class AddUserServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	//页面传来的数据：
+	//data:"userAccount="+userAccount+"&userPwd="+userPwd+"&userName="+userName+"&userSex="+userSex
+
+	}
+
+}
