f80d339c56eec9018f03a4acb88fe1cfb2969a39
wangchao <261668497@qq.com>
2018-01-26 14:24:49 +0800

    11

---
+++ dtw/src/cn/dtw/web/servlet/ShowUserServlet.java
@@ -0,0 +1,29 @@
+package cn.dtw.web.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.dtw.entity.User;
+import cn.dtw.service.UserService;
+import cn.dtw.service.impl.UserServiceImpl;
+
+public class ShowUserServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//查询员工列表
+		UserService userService = new UserServiceImpl();
+		List<User> userList = userService.getAllUser();
+	}
+
+}
