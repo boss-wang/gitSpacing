710c0390ff45c5b861cf8d57fb4d17fd5615d8f8
zongsiyao <Almost@Almost.mshome.net>
2018-03-01 16:07:56 +0800

    zz

--- dtw/src/cn/dtw/dao/customerdao/CustomerCostDao.java
+++ dtw/src/cn/dtw/dao/customerdao/CustomerCostDao.java
@@ -2,10 +2,10 @@ package cn.dtw.dao.customerdao;
 
 import java.util.List;
 
-import cn.dtw.entity.Client;
+import cn.dtw.entity.Customer;
 import cn.dtw.entity.Order_cost;
 
 public interface CustomerCostDao {
 	//通过客户id查询其应付信息，并加入订单信息
-	public List<Order_cost> getCustomerCostByClientId(Client client);
+	public List<Order_cost> getCustomerCostByClientId(Customer customer);
 }
--- dtw/src/cn/dtw/dao/customerdao/impl/CustomerCostDaoImpl.java
+++ dtw/src/cn/dtw/dao/customerdao/impl/CustomerCostDaoImpl.java
@@ -2,19 +2,22 @@ package cn.dtw.dao.customerdao.impl;
 
 import java.util.List;
 
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
 import cn.dtw.dao.BaseDao;
 import cn.dtw.dao.customerdao.CustomerCostDao;
 import cn.dtw.entity.Client;
+import cn.dtw.entity.Customer;
 import cn.dtw.entity.Order_cost;
 
 public class CustomerCostDaoImpl extends BaseDao implements CustomerCostDao {
 	
 	//通过客户id查询其应收信息，并加入订单信息
 	@Override
-	public List<Order_cost> getCustomerCostByClientId(Client client) {
+	public List<Order_cost> getCustomerCostByClientId(Customer customer) {
 		// TODO Auto-generated method stub
-		String sql="";
-		return null;
+		String sql="SELECT orderId,client.clientName,cost,invoiceNo,coststatus.`name` FROM order_cost INNER JOIN client ON order_cost.clientId=client.clientId INNER JOIN coststatus ON order_cost.costStatus=coststatus.id WHERE order_cost.clientId=(SELECT customer_client.clientId FROM customer INNER JOIN customer_client ON customer.id=customer_client.customerId WHERE customer.id=?)";
+		return super.executeQuery(new BeanListHandler<Order_cost>(Order_cost.class),sql,customer.getId());
 	}
 
 }
